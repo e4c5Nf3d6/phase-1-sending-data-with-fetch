f0bab21439aa4e96eff9fb988040fb29b99ff8c0
Maria Jenkins <macjenkin@gmail.com>
2023-04-14 05:23:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(nameStr, emailStr) {
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: nameStr,
+            email: emailStr
+        })
+    }
+
+    function handleResponse(obj) {
+        const p = document.createElement("p");
+        p.textContent = obj.id;
+        document.querySelector("body").appendChild(p);
+    }
+
+    function handleError(error) {
+        const p = document.createElement("p");
+        p.textContent = error.message;
+        document.querySelector("body").appendChild(p); 
+    }
+
+    return fetch("http://localhost:3000/users", configurationObj)
+        .then(res => res.json())
+        .then(obj => handleResponse(obj))
+        .catch(error => handleError(error))
+}
